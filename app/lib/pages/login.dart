ab95bdf4950b8fe00b84fefbdc2ac1950a320697
tomas-sucena <tomas21sucena@gmail.com>
2023-03-13 21:11:13 +0000

    started working on the Login page

--- app/lib/pages/login.dart
+++ app/lib/pages/login.dart
@@ -5,23 +5,49 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blueGrey,
-      body: SafeArea(
-        child: Column(
-          children: [
-            // logo
-            Center(
-              child: Icon(
-                Icons.lock,
-                size: 100
-              ),
-            ),
-
-            // Google sign-in
-          ],
-        ),
+    return Container(
+      decoration: BoxDecoration(
+      gradient : LinearGradient(
+          begin: Alignment(0,1),
+          end: Alignment(-1,0),
+          colors: [
+            Color.fromRGBO(248, 240, 167, 1),
+            Color.fromRGBO(232, 159, 135, 1)
+          ]
+        )
       ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SafeArea(
+          child: Column(
+            children: [
+              const SizedBox(height: 180),
+
+              // logo
+              Center(
+                child: Image.asset(
+                  "assets/images/Logo.png",
+                  height: 150),
+              ),
+
+              // name
+              const Text('CHARITY.NET',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color.fromRGBO(0, 0, 0, 1),
+                  fontFamily: 'Dokdo',
+                  fontSize: 55,
+                  letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+                  fontWeight: FontWeight.normal,
+                  height: 1
+                ),
+              )
+
+              // Google sign-in
+            ],
+          ),
+        ),
+      )
     );
   }
 }
