02dfa84821e702dd6f52643a90f216b363cbb71e
LeM0n17 <93678059+LeM0n17@users.noreply.github.com>
2023-03-22 12:05:49 +0000

    tried fixing firebase

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -4,7 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:app/pages/homepage.dart';
 import 'package:app/pages/login.dart';
 
-void main(){
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const CharityNet());
 }
 
