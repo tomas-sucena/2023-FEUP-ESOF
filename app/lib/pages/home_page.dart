4c65bd55933c457ff7cda1e57b2520360541fd34
tomas-sucena <tomas21sucena@gmail.com>
2023-04-23 21:14:41 +0100

    updated HomePage
    added a search bar at the top

--- app/lib/pages/home_page.dart
+++ app/lib/pages/home_page.dart
@@ -12,34 +12,43 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        actions: [
-          Consumer<ThemeManager>(
-            builder: (context, themeManager, child) {
-              return IconButton(
-                icon: themeManager.getIcon(),
-                onPressed: () => themeManager.switchTheme(),
-              );
-            },
-          ),
-        ],
-      ),
       body: Column(
         children: [
           addVerticalSpace(50),
-          Center(
-            child: const Text(
-              'Home',
-              style: const TextStyle(
-                fontSize: 40,
-                color: Color.fromRGBO(233, 161, 136, 1),
+
+          GestureDetector(
+            child: Container(
+              height: 60,
+              width: 360,
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(233, 161, 136, 1.0),
+                borderRadius: BorderRadius.circular(15),
               ),
-            ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Consumer<ThemeManager>(
+                    builder: (context, themeManager, child) {
+                      return IconButton(
+                        icon: themeManager.getIcon(),
+                        onPressed: () => themeManager.switchTheme(),
+                      );
+                    },
+                  ),
+
+                  addHorizontalSpace(250), // future search bar
+
+                  IconButton(
+                    icon: SvgPicture.asset("assets/images/icons/COCO_Loupe.svg"),
+                    onPressed: () => {},
+                  ),
+                ],
+              ),
+            )
           ),
+
           Image.asset('assets/images/maintenance.png', height: 500),
+
           Center(
             child: ElevatedButton(
               onPressed: () {
