13697b1350ac1d93434656f6789b91079b0001b5
tomas-sucena <tomas21sucena@gmail.com>
2023-04-24 17:36:54 +0100

    unit tested SignInButton

--- app/test/components/interactive/sign_in_button_test.dart
+++ app/test/components/interactive/sign_in_button_test.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 
 import '../../../lib/components/interactive/sign_in_button.dart';
+import '../../../lib/services/authentication/authenticator.dart';
 import '../../../lib/services/authentication/google_authenticator.dart';
+
+// Mock class
+class MockAuthenticator extends Mock implements Authenticator {}
 
 void main() {
   group("Google sign-in button", () {
@@ -15,21 +20,29 @@ void main() {
         ),
       );
 
+      // the button contains the Google logo
       expect(find.image(const AssetImage("assets/images/google_logo.png")), findsOneWidget);
+
+      // the button contains some text
       expect(find.text('Sign in with Google'), findsOneWidget);
     });
 
-    testWidgets('should trigger the authenticator when pressed', (tester) async {
+    testWidgets('should trigger the authenticator when tapped', (tester) async {
+      var _authenticator = MockAuthenticator();
+      when(_authenticator.signIn()).thenReturn("Signing in with Google");
+
       await tester.pumpWidget(
         SignInButton(
-          authenticator: GoogleAuthenticator(),
+          authenticator: _authenticator,
           logoFilename: "google_logo.png",
           name: "Google",
         ),
       );
 
-      // tap the button
+      await tester.tap(find.text('Sign in with Google'));
 
+      // the authenticator attempts to sign the user in
+      verify(_authenticator.signIn()).called(1);
     });
   });
 }
