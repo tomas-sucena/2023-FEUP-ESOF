b4c4f5355cb252bd0b886a085a197d86441a3879
tomas-sucena <tomas21sucena@gmail.com>
2023-05-10 21:32:06 +0100

    updated ProfileCard
    made it clickable

--- app/lib/components/interactive/event_card.dart
+++ app/lib/components/interactive/event_card.dart
@@ -10,50 +10,53 @@ class EventCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      width: 360,
-      decoration: BoxDecoration(
-        color: const Color.fromRGBO(232, 232, 232, 1.0),
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            color: Color.fromRGBO(0, 0, 0, 0.25),
-            offset: Offset(0,2),
-            blurRadius: 2,
-          ),
-        ],
-      ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          addHorizontalSpace(12),
-          const ProfilePicture(
-            image: AssetImage("assets/images/pedroanime.png"),
-            size: 80,
-          ),
-          addHorizontalSpace(20),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              addVerticalSpace(8),
-              Text(
-                "Save the turtles!",
-                style: Theme.of(context).textTheme.displayMedium?.copyWith(
-                  fontSize: 32,
+    return InkWell(
+      onTap: () => {},
+      child: Container(
+        height: 100,
+        width: 360,
+        decoration: BoxDecoration(
+          color: const Color.fromRGBO(232, 232, 232, 1.0),
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromRGBO(0, 0, 0, 0.25),
+              offset: Offset(0,2),
+              blurRadius: 2,
+            ),
+          ],
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            addHorizontalSpace(12),
+            const ProfilePicture(
+              image: AssetImage("assets/images/pedroanime.png"),
+              size: 80,
+            ),
+            addHorizontalSpace(20),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                addVerticalSpace(8),
+                Text(
+                  "Save the turtles!",
+                  style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                    fontSize: 32,
+                  ),
                 ),
-              ),
-              addVerticalSpace(5),
-              IconText(
-                icon: SvgPicture.asset("assets/images/icons/COCO_Profile.svg", height: 24,),
-                text: Text(
-                  "Projeto TAMAR",
-                  style: Theme.of(context).textTheme.displaySmall,
+                addVerticalSpace(5),
+                IconText(
+                  icon: SvgPicture.asset("assets/images/icons/COCO_Profile.svg", height: 24,),
+                  text: Text(
+                    "Projeto TAMAR",
+                    style: Theme.of(context).textTheme.displaySmall,
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
