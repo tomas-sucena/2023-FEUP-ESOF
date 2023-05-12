55c0266074961f2de7b93a8353cd058783054e4b
tomas-sucena <tomas21sucena@gmail.com>
2023-05-12 16:39:00 +0100

    updated NavigationPage
    fixed an issue that caused the app to be dismissed when the back button was pressed

--- app/lib/pages/navigation_page.dart
+++ app/lib/pages/navigation_page.dart
@@ -26,7 +26,7 @@ class NavigationPage extends StatefulWidget {
 
 class _NavigationPageState extends State<NavigationPage> {
   late int _currIndex;
-  final List<Key> _keys;
+  final List<GlobalKey<NavigatorState>> _keys;
 
   /* CONSTRUCTOR */
   _NavigationPageState()
@@ -51,66 +51,69 @@ class _NavigationPageState extends State<NavigationPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          PageNavigator(
-            page: HomePage(),
-            key: _keys[0],
-            isActive: _currIndex == 0,
-          ),
-          PageNavigator(
-            page: NotificationsPage(),
-            key: _keys[1],
-            isActive: _currIndex == 1,
-          ),
-          PageNavigator(
-            page: ProfilePage(widget._volunteer, widget._dbManager),
-            key: _keys[2],
-            isActive: _currIndex == 2,
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currIndex,
-        onTap: _changePage,
-        items: [
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              'assets/images/icons/COCO_Home.svg',
-              height: 38,
+    return WillPopScope(
+      onWillPop: () async => !await _keys[_currIndex].currentState!.maybePop(),
+      child: Scaffold(
+        body: Stack(
+          children: [
+            PageNavigator(
+              page: HomePage(),
+              key: _keys[0],
+              isActive: _currIndex == 0,
             ),
-            activeIcon: SvgPicture.asset(
-              'assets/images/icons/COCO_Home_active.svg',
-              height: 38,
+            PageNavigator(
+              page: NotificationsPage(),
+              key: _keys[1],
+              isActive: _currIndex == 1,
             ),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              'assets/images/icons/COCO_Notifications.svg',
-              height: 38,
+            PageNavigator(
+              page: ProfilePage(widget._volunteer, widget._dbManager),
+              key: _keys[2],
+              isActive: _currIndex == 2,
             ),
-            activeIcon: SvgPicture.asset(
-              'assets/images/icons/COCO_Notifications_active.svg',
-              height: 38,
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currIndex,
+          onTap: _changePage,
+          items: [
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/images/icons/COCO_Home.svg',
+                height: 38,
+              ),
+              activeIcon: SvgPicture.asset(
+                'assets/images/icons/COCO_Home_active.svg',
+                height: 38,
+              ),
+              label: 'Home',
             ),
-            label: 'Notifications',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              'assets/images/icons/COCO_Profile.svg',
-              height: 38,
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/images/icons/COCO_Notifications.svg',
+                height: 38,
+              ),
+              activeIcon: SvgPicture.asset(
+                'assets/images/icons/COCO_Notifications_active.svg',
+                height: 38,
+              ),
+              label: 'Notifications',
             ),
-            activeIcon: SvgPicture.asset(
-              'assets/images/icons/COCO_Profile_active.svg',
-              height: 38,
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/images/icons/COCO_Profile.svg',
+                height: 38,
+              ),
+              activeIcon: SvgPicture.asset(
+                'assets/images/icons/COCO_Profile_active.svg',
+                height: 38,
+              ),
+              label: 'Profile',
             ),
-            label: 'Profile',
-          ),
-        ],
-        selectedFontSize: 14,
-        unselectedFontSize: 12,
+          ],
+          selectedFontSize: 14,
+          unselectedFontSize: 12,
+        ),
       ),
     );
   }
